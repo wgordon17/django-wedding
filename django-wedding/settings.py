c889a2c7221efa74696866a54977859a28fbb0e8
wgordon17 <will@thegordons.me>
2016-09-24 15:56:48 -0400

    provides fallback SECRET_KEY to be used when building docker image

--- django-wedding/settings.py
+++ django-wedding/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'eh7h4f87hf8h8f7h8907shgf78hser9g78sher98h78ghsdyugsidufghs078fg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

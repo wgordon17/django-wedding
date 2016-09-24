9edd7faabd2d4639b1ea01775c3ed57a16de7ae3
wgordon17 <will@thegordons.me>
2016-09-24 16:53:07 -0400

    utilize STATICFILES_STORAGE to provide static file serving

--- django-wedding/settings.py
+++ django-wedding/settings.py
@@ -82,6 +82,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static', 'collected_static')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )

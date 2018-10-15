7f0d14501dcb79a4b52897da96f82202813af03d
Will Gordon <will@thegordons.me>
2018-10-15 15:39:12 +0000

    Update Django and migrate to Fedora/systemd

--- Wedding/models.py
+++ Wedding/models.py
@@ -4,32 +4,32 @@ from django.dispatch import receiver
 
 
 class Article(models.Model):
-	title = models.CharField(max_length=30)
-	content = models.TextField()
-	background_image = models.FileField(upload_to='Wedding/backgrounds')
-	order = models.PositiveSmallIntegerField(default=0, help_text='This value is used in determining the order in which to display individual pages')
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    background_image = models.FileField(upload_to='Wedding/backgrounds')
+    order = models.PositiveSmallIntegerField(default=0, help_text='This value is used in determining the order in which to display individual pages')
 
-	def title_id(self):
-		return self.title.replace(" ", "_").lower()
+    def title_id(self):
+        return self.title.replace(" ", "_").lower()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	class Meta():
-		ordering = ['order']
+    class Meta():
+        ordering = ['order']
 
 
 @receiver(pre_save, sender=Article)
 def article_remove_old_pics_on_save(sender, **kwargs):
-	instance = kwargs['instance']
-	if instance.id is not None:
-		prev_image = sender.objects.get(id=instance.id).background_image
-		storage, path = prev_image.storage, prev_image.path
-		storage.delete(path)
+    instance = kwargs['instance']
+    if instance.id is not None:
+        prev_image = sender.objects.get(id=instance.id).background_image
+        storage, path = prev_image.storage, prev_image.path
+        storage.delete(path)
 
 @receiver(post_delete, sender=Article)
 def article_remove_old_pics_on_delete(sender, **kwargs):
-	instance = kwargs['instance']
-	prev_image = instance.background_image
-	storage, path = prev_image.storage, prev_image.path
-	storage.delete(path)
+    instance = kwargs['instance']
+    prev_image = instance.background_image
+    storage, path = prev_image.storage, prev_image.path
+    storage.delete(path)
--- django-wedding/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls import patterns, include, url, static
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^', include('Wedding.urls', namespace='Wedding')),
-    url(r'^admin/', include(admin.site.urls)),
-    # Comment out before '+'
-) + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
